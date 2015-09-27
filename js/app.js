9886ac468e96ad3deb88665f3c6c59574cf33481
Howard Yeh <howard@metacircus.com>
2015-09-27 20:00:28 +0800

    buyshoes layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+function makeCartScrollNicely() {
+  var cart = document.querySelector(".cart__content");
+  Ps.initialize(cart);
+}
+
 window.onload = function() {
+  makeCartScrollNicely();
   console.log("page loaded");
 }
