14f78810eadf8bd2fcefde471c5607cc7577c26f
Howard Yeh <howard@metacircus.com>
2015-09-30 10:24:22 +0800

    more layout stuff

--- js/app.js
+++ js/app.js
@@ -3,7 +3,16 @@ function makeCartScrollNicely() {
   Ps.initialize(cart);
 }
 
+function addSidebarToggle() {
+  // var $sidebar = document.querySelector(".site__right-sidebar");
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
+}
+
 window.onload = function() {
   makeCartScrollNicely();
+  addSidebarToggle();
   console.log("page loaded");
 }
